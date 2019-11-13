9f2ce78fef115741b182fa728f9676005ff5ab3e
DudaGod <dudkevich@yandex-team.ru>
2019-11-13 22:38:12 +0300

    feat: first implementation

---
+++ test/lib/repeater.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Repeater = require('lib/repeater');
+
+describe('Repeater', () => {
+    let hermione;
+
+    const _mkTest = ({browserId = 'default-bro'} = {}) => ({browserId});
+    const _mkTestCollection = (tests) => ({eachTest: (cb) => tests.forEach(cb)});
+    const _mkRepeatCounter = () => ({addTest: sinon.stub()});
+
+    beforeEach(() => {
+        hermione = {addTestToRun: sinon.stub()};
+    });
+
+    afterEach(() => sinon.restore());
+
+    describe('"repeat" method', () => {
+        it('should register each test in repeat counter', () => {
+            const repeatCounter = _mkRepeatCounter();
+            const repeater = Repeater.create(hermione, repeatCounter);
+            const tests = [_mkTest(), _mkTest()];
+            repeater.setTestCollection(_mkTestCollection(tests));
+
+            repeater.repeat(1);
+
+            assert.calledTwice(repeatCounter.addTest);
+            assert.calledWith(repeatCounter.addTest.firstCall, tests[0]);
+            assert.calledWith(repeatCounter.addTest.secondCall, tests[1]);
+        });
+
+        it('should repeat test the specified number of repeats', () => {
+            const repeater = Repeater.create(hermione, _mkRepeatCounter());
+            const test = _mkTest({browserId: 'yabro-1'});
+            repeater.setTestCollection(_mkTestCollection([test]));
+
+            repeater.repeat(2);
+
+            assert.calledTwice(hermione.addTestToRun);
+            assert.calledWith(hermione.addTestToRun.firstCall, test, test.browserId);
+            assert.calledWith(hermione.addTestToRun.secondCall, test, test.browserId);
+        });
+    });
+});
